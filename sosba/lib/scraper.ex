01f301b8249bde64ca3312037e61ec20d6d9b02c
Daniel Münch <dani.munch@gmail.com>
2015-12-30 00:17:56 +0100

    First draft of automatic booking.

--- sosba/lib/scraper.ex
+++ sosba/lib/scraper.ex
@@ -1,6 +1,7 @@
 defmodule Scraper do
   use Timex 
   @base_url "https://service.berlin.de/terminvereinbarung/termin/"
+  @url_booking "https://service.berlin.de/terminvereinbarung/termin/bestaetigung.php"
   @baseDaysDecember2015 1448924400
 
   @baseUrlDate "https://service.berlin.de/terminvereinbarung/termin/tag.php?id=&buergerID=&buergername=&absagecode=&Datum={date}&anliegen%5B%5D=120703&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&herkunft=/terminvereinbarung/"
@@ -121,12 +122,13 @@ defmodule Scraper do
 
     cookie = get_auth_cookie
     process_timetable = fn(date) -> 
-      #cookie = get_auth_cookie
+      cookie = get_auth_cookie
       load(@base_url <> date.url, cache_control, cookie)
       |> save_timetable(date)
       |> parse_timetable
       |> List.flatten
       |> Enum.map(&Dict.put(&1, :date, date.date))
+      |> Enum.map(&Dict.put(&1, :cookie, cookie))
     end
 
     appointements = dates 
@@ -165,7 +167,7 @@ defmodule Scraper do
               time: url |> parse_zeit,
               oid: url |> parse_oid,
               anliegen: url |> parse_anliegen,
-              #url: url, 
+              url: @base_url <> url, 
               name: of |> Floki.text
             }
           end
@@ -179,5 +181,27 @@ defmodule Scraper do
     url
     |> URI.decode_query 
     |> (fn map -> map[field] end).() 
+  end
+
+  def book_appointement(appt) do
+    form = load(appt.url, "", appt.cookie)
+    |> parse_appointement_page
+    |> Dict.put(:EMail, "coun1932@gustr.com")
+    |> Dict.put(:Nachname, "siggi")   
+    #telefonnummer_fuer_rueckfragen 
+    
+    HTTPoison.post(@url_booking, {:form, form},  
+                   %{"Content-type" => "application/x-www-form-urlencoded", "Accept" => "text/html"},  
+                   [hackney: [cookie: [{"ZMS-BO_Webinterface", appt.cookie}]]])       
+  end
+  
+  def parse_appointement_page(html) do
+    html  
+    |> Floki.find("form")
+    |> Enum.at(1) 
+    |> Floki.find("input")
+    |> Enum.filter(&Floki.attribute(&1, "type") |> hd != "submit")
+    |> Enum.map(&%{name: Floki.attribute(&1, "name") |> hd, value: Floki.attribute(&1, "value") |> hd })
+    |> Enum.reduce([], fn(x, acc) -> Dict.put(acc, String.to_atom(x.name), x.value) end) 
   end
 end
