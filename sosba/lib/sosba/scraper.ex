2eddb88fad1b19277e5b8ee57ce3510e39d2e0aa
Daniel Münch <dani.munch@gmail.com>
2016-01-05 09:07:03 +0100

    Implement scraping for multiple "anliegen".

--- sosba/lib/sosba/scraper.ex
+++ sosba/lib/sosba/scraper.ex
@@ -4,16 +4,27 @@ defmodule Scraper do
   @url_booking "https://service.berlin.de/terminvereinbarung/termin/bestaetigung.php"
   @baseDaysDecember2015 1448924400
 
-  @baseUrlDate "https://service.berlin.de/terminvereinbarung/termin/tag.php?id=&buergerID=&buergername=&absagecode=&Datum={date}&anliegen%5B%5D=120703&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&herkunft=/terminvereinbarung/"
+  @baseUrlDate "https://service.berlin.de/terminvereinbarung/termin/tag.php?id=&buergerID=&buergername=&absagecode=&Datum={date}&anliegen%5B%5D={anliegen}&dienstleister%5B%5D=122210&dienstleister%5B%5D=122217&dienstleister%5B%5D=122219&dienstleister%5B%5D=122227&dienstleister%5B%5D=122231&dienstleister%5B%5D=122238&dienstleister%5B%5D=122243&dienstleister%5B%5D=122252&dienstleister%5B%5D=122260&dienstleister%5B%5D=122262&dienstleister%5B%5D=122254&dienstleister%5B%5D=122271&dienstleister%5B%5D=122273&dienstleister%5B%5D=122277&dienstleister%5B%5D=122280&dienstleister%5B%5D=122282&dienstleister%5B%5D=122284&dienstleister%5B%5D=122291&dienstleister%5B%5D=122285&dienstleister%5B%5D=122286&dienstleister%5B%5D=122296&dienstleister%5B%5D=150230&dienstleister%5B%5D=122301&dienstleister%5B%5D=122297&dienstleister%5B%5D=122294&dienstleister%5B%5D=122312&dienstleister%5B%5D=122314&dienstleister%5B%5D=122304&dienstleister%5B%5D=122311&dienstleister%5B%5D=122309&dienstleister%5B%5D=317869&dienstleister%5B%5D=324433&dienstleister%5B%5D=325341&dienstleister%5B%5D=324434&dienstleister%5B%5D=324435&dienstleister%5B%5D=122281&dienstleister%5B%5D=324414&dienstleister%5B%5D=122283&dienstleister%5B%5D=122279&dienstleister%5B%5D=122276&dienstleister%5B%5D=122274&dienstleister%5B%5D=122267&dienstleister%5B%5D=122246&dienstleister%5B%5D=122251&dienstleister%5B%5D=122257&dienstleister%5B%5D=122208&dienstleister%5B%5D=122226&herkunft=/terminvereinbarung/"
  
+  @anliegen_personalausweis 120703
+  @anliegen_wohnung 120686
+  @anliegen_reisepass 121151 
+
+
   def load_base do 
-    load_for_date(@url, {2015, 12, 1})
+    load_for_date_and_anliegen(@url, @anliegen_personalausweis, {2015, 12, 1})
   end
  
-  def load_for_date(date, cache_control) do
+  def load_for_date_and_anliegen_async(date, anliegen, cache_control) do
+   Task.async(fn -> load_for_date_and_anliegen(date, anliegen, cache_control) end)
+  end
+  
+  def load_for_date_and_anliegen(date, anliegen, cache_control) do
     days = @baseDaysDecember2015 + Date.diff(Date.from({2015,12,1}), Date.from(date), :secs)
 
     url = String.replace(@baseUrlDate, "{date}", Integer.to_string(days))
+    url = String.replace(url, "{anliegen}", Integer.to_string(anliegen))
+
     load(url, cache_control)
   end
 
@@ -108,10 +119,17 @@ defmodule Scraper do
     Date.now |> DateFormat.format("{YYYY}-{M}-{D}-{h24}-{m}-{s}")
 
     dates = [
-      {2015, 12, 1},
-      {2016,  2, 1}
+      {2016, 1, 1},
+      {2016, 3, 1}
     ]
-    |> Enum.map(&Task.async(fn -> load_for_date(&1, cache_control) end))
+    |> Enum.flat_map(fn date -> [
+      #Task.async(&load_for_date_and_anliegen(&1, @anliegen_personalausweis, cache_control)),
+      # Task.async(&load_for_date_and_anliegen(&1, @anliegen_wohnung, cache_control)),
+      # Task.async(&load_for_date_and_anliegen(&1, @anliegen_reisepass, cache_control)),
+        load_for_date_and_anliegen_async(date, @anliegen_personalausweis, cache_control),
+        load_for_date_and_anliegen_async(date, @anliegen_wohnung, cache_control),
+        load_for_date_and_anliegen_async(date, @anliegen_reisepass, cache_control)
+    ] end)
     |> Enum.map(&Task.await(&1, 20000))
     |> Enum.map(&parse_two_months/1)
     |> Enum.reduce([], fn(x, acc) -> acc ++ x end)
